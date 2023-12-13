63db91fd8bf0be9513fafd88636898636b3a9c23
Michael Yarborough <michael.yarborough@icloud.com>
2023-12-13 16:22:25 +0000

    Test

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
     name = getenv('YOUR_NAME')
     if name is None:
       name = "No Name Selected"
-    return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n<h2>Good2Bye {name}.</h2\n<h3>Good Bye {name}.</h3>"
+    return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n"
 if __name__=='__main__':
   app.run(host='0.0.0.0', port=5500, debug=True)
 
